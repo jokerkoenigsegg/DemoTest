364e6b0df3bc49cbc3f4f22208f96411ecc4ff19
jokerkoenigsegg <52539457+jokerkoenigsegg@users.noreply.github.com>
2020-02-12 17:10:33 +0800

    update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,9 +1,9 @@
 public class Demo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("请输入一个整数：");
+		System.out.println("PLS input a number:");
 		int a = sc.nextInt();
-		System.out.println("请再输入一个整数：");
+		System.out.println("PLS input other number:");
 		int b = sc.nextInt();
 		if(a > b) {
 			System.out.println("YOU WIN!");
