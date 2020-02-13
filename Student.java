a4aa52d75b3510b43be74eefdfc838e5a2d5bff3
joker <joker5558@outlook.com>
2020-02-13 11:05:51 +0800

    add a pojo Student.java

---
+++ Student.java
@@ -0,0 +1,83 @@
+public class Student {
+    private String name;
+    private Integer age;
+    private Double height;
+    private String[] hobbies;
+
+    public Student() {
+        super();
+    }
+
+    public Student(String name, Integer age, Double height, String[] hobbies) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.hobbies = hobbies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        if (age != null ? !age.equals(student.age) : student.age != null) return false;
+        if (height != null ? !height.equals(student.height) : student.height != null) return false;
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        return Arrays.equals(hobbies, student.hobbies);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (height != null ? height.hashCode() : 0);
+        result = 31 * result + Arrays.hashCode(hobbies);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", hobbies=" + Arrays.toString(hobbies) +
+                '}';
+    }
+}
